9fd73e0515dd9e5eeede57d8ae39215a273a08aa
LEARN Academy <learnacademy@MacBook-Air-3.local>
2024-01-08 11:17:32 -0800

    create react app with appriate seed data and dependencies/ requesting branch protection

---
+++ src/mockBasketballCourts.js
@@ -0,0 +1,36 @@
+let mockBasketBallCourts = [
+    {
+      id: 1,
+      name: "Court 1",
+      address: "123 Happy Street",
+      city: "City One",
+      state: "State 1",
+      zip: 12345,
+      court_type: "indoor",
+      number_players: 5,
+      user_id: 1,
+    },
+    {
+      id: 2,
+      name: "Court 2",
+      address: "1234 Sad Street",
+      city: "City Two",
+      state: "State 2",
+      zip: 54321,
+      court_type: "outdoor",
+      number_players: 4,
+      user_id: 2,
+    },
+    {
+      id: 3,
+      name: "Court 3",
+      address: "321 Goat Ave",
+      city: "City Three",
+      state: "State 3",
+      zip: 67890,
+      court_type: "rubber",
+      number_players: 2,
+      user_id: 1,
+    },
+  ];
+  export default mockBasketBallCourts;
